2fe737ed9878c76b61296436b1ed7b3489f9310b
chadcarlson <chad.carlson@platform.sh>
2020-10-15 11:03:43 -0400

    platformify with npm.

--- prismic-configuration.js
+++ prismic-configuration.js
@@ -1,7 +1,7 @@
 // -- Prismic API endpoint
 // Determines which repository to query and fetch data from
 // Configure your site's access point here
-export const apiEndpoint = 'https://your-repo-name.cdn.prismic.io/api/v2'
+export const apiEndpoint = `${process.env.API_ENDPOINT}/api/v2` || 'https://your-repo-name.cdn.prismic.io/api/v2'
 
 // -- Access Token if the repository is not public
 // Generate a token in your dashboard and configure it here if your repository is private
